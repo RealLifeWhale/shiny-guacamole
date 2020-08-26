36edec9571d88bfaf5bff9bb0f9f65328a276ada
Someone <38514929+RealLifeWhale@users.noreply.github.com>
2020-08-26 01:17:15 -0600

    Hello world. I do not vibe with this universe but hey my code works!

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+//I do not vibe with this universe however
+//I will still say hello, as it is polite.
+int main(int argc, char const *argv[]) {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
