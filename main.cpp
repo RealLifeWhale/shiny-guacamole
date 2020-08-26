0753bf738a3e7c36efa8439a6cf02740dd8e6d4f
Someone <38514929+RealLifeWhale@users.noreply.github.com>
2020-08-26 02:09:15 -0600

    Oh my god it works it really works I did it! Oh, uh code makes window...

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
+//WHAT THE FUCK HOW DO I CODE??
+//Oh nevermind I figured it out.
 
-//I do not vibe with this universe however
-//I will still say hello, as it is polite.
+//So I guess I'm making a game with SDL2 cause pain
+//is my middle name. include it. And other libs?
+#include <SDL2/SDL.h>
+
+//Now that I made it clear I have no clue what
+//I'm doing, I will make the code do stuff.
 int main(int argc, char const *argv[]) {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+  //GAMER BOY WHY YOU DO THIS TO ME??
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Window* window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 800, SDL_WINDOW_SHOWN);
+  SDL_Delay(3000);
+  return 0; //This is exit with no error!
 }
+
+//abcdefghijklmnopqrstuvwxyz
+//NEXT TIME WONT YOU SING IT WITH ME?!
