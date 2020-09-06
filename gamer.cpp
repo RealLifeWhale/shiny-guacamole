da715db63cb0a02e3cca627de36d1eb3610fa164
Someone <38514929+RealLifeWhale@users.noreply.github.com>
2020-09-05 19:08:24 -0600

    Was real sick of composter geting disabled so fixed that, did flags too.

--- gamer.cpp
+++ gamer.cpp
@@ -1,11 +1,12 @@
 // Include the gamer class
 #include "gamer.h"
 
-gamer::gamer(/* args */int width, int height) {
-    if (!SDL_Init(SDL_INIT_EVERYTHING))
-    {
-        window = SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_RESIZABLE);
-    }
+gamer::gamer(/* args */int width, int height, Uint32 flags) {
+    if (SDL_Init(SDL_INIT_EVERYTHING)) {SDL_Log("Failed"); return;}
+    SDL_Log("Initalized subsystems.");
+    // So the composter is cool cause animations and this will make it so its not disabled.
+    SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0");
+    window = SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, flags);
     
 }
 
