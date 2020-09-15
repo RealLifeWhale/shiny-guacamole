1f1f3ecd4cbac05b3cb042074a4f92bc7e0ff092
Someone <38514929+RealLifeWhale@users.noreply.github.com>
2020-09-14 23:31:27 -0600

    ([ENTER] [TAB]) x2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 
 // It's main function runs at start
 int main(int argc, char const *argv[]) {
+    for(int i = 0; i < argc; i++) {
+        if(argv[i] == "-h") {
+            SDL_Log("Hello World!");
+        }
+    }
     gamer* game = new gamer(600, 800, SDL_WINDOW_RESIZABLE);
     while(game->isRunning())
     {
